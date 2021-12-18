38a7e44c7e69ef333b81b5e6021248de05f7a142
tazz0009 <tazz0009@gmail.com>
2021-12-19 08:09:07 +0900

    chap02. SpringSecurity 세팅 - token 확인
    1. CustomAuthorizationFilter 추가 - head 내의 token을 확인하는 필터

--- README.md
+++ README.md
@@ -8,17 +8,24 @@
 1. user / Using generated security password로 로그인 
 1. DB에 있는 user 정보가 나온다.
 
-## chap01. SpringSecurity 세팅
+## chap01. SpringSecurity 세팅 - token 생성
 
 1. CorsFilter 생성
 1. SecurityConfig 생성
 1. UserService에 UserDetailsService 추가
 1. CustomAuthenticationFilter 추가
 
-- test 
+- postman test 
 1. localhost:8080/api/users 접속
 1. post username:tazz001 / password: 1234 로 로그인 
 1. access_token, refresh_token 확인가능
 1. https://jwt.io/ 토큰 내용 확인가능
 
+## chap02. SpringSecurity 세팅 - token 확인
 
+1. CustomAuthorizationFilter 추가 - head 내의 token을 확인하는 필터
+
+- postman test 
+1. localhost:8080/api/users 접속 - 403 에러
+1. localhost:8080/api/users 헤더에 Authorization 에 bear 임의의 문자 입력 - 403 에러
+1. localhost:8080/api/users 헤더에 Authorization 에 bear JWT 문자 입력 - 정상출력
--- src/main/java/com/tazz009/jwttest/config/SecurityConfig.java
+++ src/main/java/com/tazz009/jwttest/config/SecurityConfig.java
@@ -1,9 +1,8 @@
 package com.tazz009.jwttest.config;
 
-import javax.servlet.Filter;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,8 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.tazz009.jwttest.filter.CustomAuthenticationFilter;
+import com.tazz009.jwttest.filter.CustomAuthorizationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -33,12 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
-		
+		customAuthenticationFilter.setFilterProcessesUrl("/api/login");
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().antMatchers("/api/login/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/api/users/**").hasAnyAuthority("ROLE_USER");
+		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/user/save/**").hasAnyAuthority("ROLE_ADMIN");
+		http.authorizeRequests().anyRequest().authenticated();
 
 		http.addFilter(customAuthenticationFilter);
+		http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean
